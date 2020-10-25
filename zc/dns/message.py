a1d92ca6e565c0a6ba5fae9d9d3f179073e9ba1b
Staffan Thomen <duck@shangtai.net>
2020-10-25 12:40:14 +0200

    Separate input records into sections for easier encoding later

--- zc/dns/message.py
+++ zc/dns/message.py
@@ -8,46 +8,66 @@ from .query import Query
 class Message(Data):
 	FORMAT = '!HHHHHH'
 
+	SEC_QUESTION   = 0
+	SEC_ANSWER     = 1
+	SEC_NS         = 2
+	SEC_ADDITIONAL = 3
+
 	def decode(self):
 		"""
 		Decode a DNS message
 
 		:param raw bytes: The raw message
 		"""
-		# Unpack the header
-
 		(
 			self.id,
 			flags,
-			self.zcount,
-			self.pcount,
-			self.ucount,
-			self.acount
+			self.qcount,	# Question
+			self.acount,	# Answer
+			self.ncount,	# Nameservers
+			self.xcount		# Additional records
 		) = unpack(self.FORMAT, self.raw[:12])
 
-		# Initialize flags
 		self.flags = Flags(flags)
-
-		# Load the records
-
-		self.records = []
 
 		offset=12
 		self.length = len(self.raw)
 
-		for r in range(0, (self.zcount + self.pcount + self.ucount + self.acount)):
-			if self.flags.qr:
-				record = Record(self.raw, offset)
-			else:
+		self.records = {
+			self.SEC_QUESTION:   [],
+			self.SEC_ANSWER:     [],
+			self.SEC_NS:         [],
+			self.SEC_ADDITIONAL: []
+		}
+
+		breakpoints = []
+		total = 0
+		for idx, cur in enumerate([ self.qcount, self.acount, self.ncount, self.xcount ]):
+			breakpoints.append(cur + total)
+			total += cur
+
+		section = self.SEC_QUESTION
+
+		for index in range(0, total):
+			if self.flags.qr != True:
 				record = Query(self.raw, offset)
+			else:
+				record = Record(self.raw, offset)
 
 			offset+=len(record)
-			self.records.append(record)
+
+			# Find the appropriate section to put this entry in
+			for section, bp in enumerate(breakpoints):
+				if index < bp:
+					break
+
+			self.records[section].append(record)
 
 		return self
 
 	def encode(self):
-		# Encode the header
+		if not self.id:
+			self.id = 0
 
 		self.raw = pack(
 			self.FORMAT,
@@ -58,8 +78,5 @@ class Message(Data):
 			self.ucount,
 			self.acount
 		)
-
-		# Encode the data parts
-		# TODO
 
 		return self
