6fb2be0ca16ca9f987a5c2afdae46d05b86b4d91
Staffan Thomen <duck@shangtai.net>
2020-10-31 14:19:07 +0200

    Add a sender and timeout to scan

--- scan.py
+++ scan.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python3.7
 
 from zc import MulticastListener
-from zc.dns import Message
+from zc.dns import Message, Query, Record, rr
 
+# Register for listening to mDNS
 listener = MulticastListener().register('224.0.0.251', 5353)
 
+# our query, are there any chromecasts out there?
+query = Query() \
+	.setLabels(b'_googlecast', b'_tcp', b'local') \
+	.setClass(Record.CLASS_IN) \
+	.setType(rr.TYPE_PTR)
+
+# Create a message and add our question
+question = Message()
+question.addRecord(Message.QUESTION, query)
+
+listener.send(question)
+
+# Loop while the answers flood in (or not)
 while True:
+
+	# listener is derived from socket, so we can set a timeout
+	listener.settimeout(1)
 	data, remote = listener.receive()
+
+	# timed out
+	if data == None:
+		break
+
 	print(f"--- START Packet from {remote}, {len(data)} bytes\n")
 	message = Message(data)
 
@@ -17,5 +39,3 @@ while True:
 			print(record)
 
 	print("\n--- END\n")
-
-listener.unregister()
